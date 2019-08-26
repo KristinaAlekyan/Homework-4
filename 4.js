ddf1537ea17526a958bd091cef3f867d29fc9d39
KristinaAlekyan <53223679+KristinaAlekyan@users.noreply.github.com>
2019-08-26 17:24:21 +0400

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,41 @@
+
+/*3.Convert a long phrase to its acronym.*/
+
+let str = 'Prisoner of War';    //let str='Have a good night'
+
+let res = str.split(" ");
+let arr=[];
+	for(let i=0; i<res.length; i++){
+	arr.push(res[i].charAt(0).toUpperCase());		
+	}  
+
+let strAcronym = arr.join('');
+console.log(strAcronym);
+
+
+
+/*4. Given a string of digits, output all the contiguous substrings of length n in that string.*/
+
+let str;
+
+let n;
+let arrNew=[];
+function subStr(str,n){
+
+
+for (let i=0; i < str.length-n+1; i ++){
+
+arrNew.push(str.slice(i,i+n));
+}
+return arrNew;
+}
+console.log(subStr('1234567',8));
+
+ 
+
+
+
+
+
+
+
